0f214b54e7b279172d7b29eecf551c5ff6644b34
sashaindrish <alexrover2020@yandex.ru>
2024-02-03 21:39:48 +0300

    добавил работу с функцией времени и добавил команду компиляции

--- leson1/main.go
+++ leson1/main.go
@@ -1,9 +1,14 @@
 package main
 
-// для инита необходимо выполнить  go mod init leson1св
-import "fmt"
+// для инита необходимо выполнить  go mod init leson1
+// go build main.go
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	name := "Go Developers"
 	fmt.Println("Azure for", name)
+	fmt.Println("System time - ", time.Now())
 }
