204a1173c92292819707302282a8c918144a244f
sashaindrish <alexrover2020@yandex.ru>
2024-02-03 16:32:26 +0300

    initial commit

---
+++ leson1/go.mod
@@ -0,0 +1,3 @@
+module leson1
+
+go 1.21.6
---
+++ leson1/main.go
@@ -0,0 +1,9 @@
+package main
+
+// для инита необходимо выполнить  go mod init leson1
+import "fmt"
+
+func main() {
+	name := "Go Developers"
+	fmt.Println("Azure for", name)
+}
